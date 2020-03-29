e4ed73f415c4429106bb8e65582ba2ad5825f7c1
Zsolti <zsol.pz@gmail.com>
2020-03-29 20:11:16 +0200

    Kötél használ

--- projgramozok/src/projgramozok/Kotel.java
+++ projgramozok/src/projgramozok/Kotel.java
@@ -5,9 +5,7 @@ public class Kotel extends Targy{
 	@Override
 	public void hasznal(Karakter k) {
 		System.out.println("-->hasznal(k)");
-		Karakter uj = k.getTabla().getszomszed(Irany.Bal).getkarakterek()[0];
-		System.out.println("ssssssssssssssss");
-		uj.lep(k.tabla);
+		k.getTabla().getszomszed(Irany.Bal).getkarakterek()[0].lep(k.tabla);
 		System.out.println("void<--");
 		
 	}

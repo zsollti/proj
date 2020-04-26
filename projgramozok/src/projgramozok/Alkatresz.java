abac54fd364c2a59c23e2abe372fa43d6e837893
macseber <nembalika@freemail.hu>
2020-04-26 20:53:11 +0200

    nullpointerfix 2.0

--- projgramozok/src/projgramozok/Alkatresz.java
+++ projgramozok/src/projgramozok/Alkatresz.java
@@ -1,6 +1,7 @@
 package projgramozok;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Alkatresz extends Targy{
 
@@ -15,9 +16,12 @@ public class Alkatresz extends Targy{
 		if(Palya.vizsgal(k)) {
 			db = 0;
 			for (Szereplo sz : Palya.szereplok) {
-				ArrayList<Targy> l = sz.getInventory().get(getName());
-				if (l != null) {
-					db += sz.getInventory().get(getName()).size();
+				HashMap<String, ArrayList<Targy>> t = sz.getInventory();
+				if ( t != null) {
+					ArrayList<Targy> l = sz.getInventory().get(getName());
+					if (l != null) {
+						db += sz.getInventory().get(getName()).size();
+					}
 				}
 			}
 			if (db == 3) Palya.gameover(null);

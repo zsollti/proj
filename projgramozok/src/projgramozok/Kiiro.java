bf6ff0eab4f9ec2dfe0ce40f41571be8396d1019
macseber <nembalika@freemail.hu>
2020-04-27 17:13:38 +0200

    Kiiro kommentek + fájl eltüntetése

--- projgramozok/src/projgramozok/Kiiro.java
+++ projgramozok/src/projgramozok/Kiiro.java
@@ -1,33 +1,23 @@
 package projgramozok;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
+/**Kiíró osztály, akár egyszerre több helyre is képes írni.
+ * 
+ */
 public class Kiiro {
 	private static boolean Konzolra = true;
-	private static FileWriter file = null;
 	
+	/**Kiírja a beállítot helyekre a szöveget.
+	 * @param s a kiírandó string
+	 */
 	public static void Kiir(String s) {
 		if(Konzolra) {
 			System.out.println(s);
 		}
-		if(file != null) {
-			try {
-				file.write(s);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 	}
+	/**Szám kiírása
+	 * @param i a kiírandó szám
+	 */
 	public static void Kiir(int i) {
 		Kiir(Integer.toString(i));
-	}
-	
-	public static void setFile(String s) {
-		try {
-			file = new FileWriter(s);
-		} catch (IOException e) {
-			System.out.println("a fájl nem található (kiiró)");
-		}
 	}
 }
